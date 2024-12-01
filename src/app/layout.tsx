2adef7d3bd621cc89d3362e4c2c77c3b6516291d
Thanapon Johdee <sern.dev@gmail.com>
2024-12-01 23:14:19 +0700

    rm: nextui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { MyNextUiProvider } from "./myNextUiProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}><MyNextUiProvider>{children}</MyNextUiProvider></body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
