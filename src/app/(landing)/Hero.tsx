7d34b9d941c3b5ff66f75d8c4a20de5b74f12605
Thanapon Johdee <sern.dev@gmail.com>
2025-02-25 00:24:47 +0700

    feat: enhance HeroSection layout and typography for improved responsiveness

--- src/app/(landing)/Hero.tsx
+++ src/app/(landing)/Hero.tsx
@@ -2,11 +2,11 @@ import { CharacterScrambleEffect } from '@/components/CharacterScrambleEffect';
 
 export default function HeroSection() {
   return (
-    <section className="container mx-auto w-full min-h-[calc(100dvh-360px)]">
-      <div className="flex flex-col my-4 gap-2 w-full text-center text-primary text-4xl transition">
+    <section className="container px-4 mx-auto w-full md:min-h-[calc(100dvh-360px)]">
+      <div className="flex flex-col my-4 gap-2 w-full text-center text-primary text-2xl md:text-4xl transition">
         <span className="block">Hi, I&apos;m</span>
         <CharacterScrambleEffect
-          className="font-bold block text-8xl"
+          className="font-bold block text-4xl md:text-8xl"
           texts={['Thanapon', 'Yoke.th']}
         />
         <span className="block">a third-year engineering student at Chulalongkorn University and a developer.</span>
