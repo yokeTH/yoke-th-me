8f0908946553f46cf2e5d121d745cf937bbb516d
Thanapon Johdee <sern.dev@gmail.com>
2025-02-25 06:40:56 +0700

    feat: implement TextLoop component for dynamic text display in HeroSection

--- src/app/(landing)/Hero.tsx
+++ src/app/(landing)/Hero.tsx
@@ -1,15 +1,22 @@
 import { CharacterScrambleEffect } from '@/components/CharacterScrambleEffect';
+import { TextLoop } from '@/components/ui/text-loop';
 
 export default function HeroSection() {
   return (
     <section className="container px-4 mx-auto w-full md:min-h-[calc(100dvh-360px)]">
-      <div className="flex flex-col my-4 gap-2 w-full text-center text-primary text-2xl md:text-4xl transition">
-        <span className="block">Hi, I&apos;m</span>
-        <CharacterScrambleEffect
-          className="font-bold block text-4xl md:text-8xl"
-          texts={['Thanapon', 'Yoke.th']}
-        />
-        <span className="block">a third-year engineering student at Chulalongkorn University and a developer.</span>
+      <div className="flex flex-col my-4 gap-2 w-full text-start text-primary text-2xl md:text-4xl transition">
+        <div className="inline">
+          <span className="">Hi, I&apos;m </span>
+          <TextLoop
+            interval={4}
+            className="font-bold"
+          >
+            <span>Thanapon Johdee</span>
+            <span>Yoke.th</span>
+          </TextLoop>
+          <br />
+          <span className=""> a third-year engineering student at Chulalongkorn University and a developer.</span>
+        </div>
       </div>
     </section>
   );
