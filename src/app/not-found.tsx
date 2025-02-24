6b2c96ee89231379932b11caf1ef659de8ed63da
Thanapon Johdee <sern.dev@gmail.com>
2025-02-24 22:00:02 +0700

    style: format

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,4 @@
-export const runtime = "edge";
+export const runtime = 'edge';
 
 export default function NotFound() {
   return (
@@ -11,7 +11,10 @@ export default function NotFound() {
               __html: `body{color:#000;background:#fff;margin:0}.next-error-h1{border-right:1px solid rgba(0,0,0,.3)}@media (prefers-color-scheme:dark){body{color:#fff;background:#000}.next-error-h1{border-right:1px solid rgba(255,255,255,.3)}}`,
             }}
           />
-          <h1 className="next-error-h1" style={styles.h1}>
+          <h1
+            className="next-error-h1"
+            style={styles.h1}
+          >
             404
           </h1>
           <div style={styles.desc}>
@@ -25,34 +28,33 @@ export default function NotFound() {
 
 const styles = {
   error: {
-    fontFamily:
-      'system-ui,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"',
-    height: "100vh",
-    textAlign: "center",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
+    fontFamily: 'system-ui,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"',
+    height: '100vh',
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
   desc: {
-    display: "inline-block",
+    display: 'inline-block',
   },
 
   h1: {
-    display: "inline-block",
-    margin: "0 20px 0 0",
-    padding: "0 23px 0 0",
+    display: 'inline-block',
+    margin: '0 20px 0 0',
+    padding: '0 23px 0 0',
     fontSize: 24,
     fontWeight: 500,
-    verticalAlign: "top",
-    lineHeight: "49px",
+    verticalAlign: 'top',
+    lineHeight: '49px',
   },
 
   h2: {
     fontSize: 14,
     fontWeight: 400,
-    lineHeight: "49px",
+    lineHeight: '49px',
     margin: 0,
   },
 } as const;
