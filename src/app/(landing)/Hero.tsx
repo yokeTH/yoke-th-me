67533433a6447446aa139a8456e6eac62e56df33
Thanapon Johdee <sern.dev@gmail.com>
2025-02-25 08:12:30 +0700

    feat: enhance HeroSection with additional text and GitHub link

--- src/app/(landing)/Hero.tsx
+++ src/app/(landing)/Hero.tsx
@@ -1,11 +1,12 @@
 import { CharacterScrambleEffect } from '@/components/CharacterScrambleEffect';
 import { TextLoop } from '@/components/ui/text-loop';
+import Link from 'next/link';
 
 export default function HeroSection() {
   return (
-    <section className="container px-4 mx-auto w-full md:min-h-[calc(100dvh-360px)]">
-      <div className=" my-4 gap-2 w-full h-fit text-start justify-center items-center text-primary text-2xl md:text-4xl transition">
-        <div className="inline">
+    <section className="container px-4 mx-auto w-full md:min-h-[calc(100dvh-360px)] flex flex-col justify-center">
+      <div className=" my-4 gap-2 w-full h-fit text-start justify-center items-center text-primary transition">
+        <div className="inline text-2xl md:text-4xl">
           <span className="">Hi, I&apos;m </span>
           <TextLoop
             interval={4}
@@ -16,6 +17,19 @@ export default function HeroSection() {
           </TextLoop>
           <br />
           <span className=""> a third-year engineering student at Chulalongkorn University and a developer.</span>
+        </div>
+        <div className="pt-8 text-xl flex flex-col gap-2">
+          <span className="block">My site's still a work in progress—stay tuned!</span>
+          <span className="block">
+            In the meantime, check out my{' '}
+            <Link
+              href="https://github.com/yokeTH"
+              className="px-4 py-2 border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200"
+            >
+              GitHub
+            </Link>{' '}
+            to see what I'm up to! 🚀
+          </span>
         </div>
       </div>
     </section>
