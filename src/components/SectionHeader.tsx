9f9ab2b7d980e3f1a2118077ea93576e51c31703
Thanapon Johdee <sern.dev@gmail.com>
2025-02-25 10:40:24 +0700

    feat: add id attribute to SectionHeader for improved accessibility and targeting

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -5,7 +5,10 @@ interface SectionHeaderProps {
 
 export function SectionHeader({ name, position = 'start' }: SectionHeaderProps) {
   return (
-    <div className={`flex flex-row${position === 'end' && '-reverse'} gap-2 items-center mb-4`}>
+    <div
+      className={`flex flex-row${position === 'end' && '-reverse'} gap-2 items-center mb-4`}
+      id={name.toLocaleLowerCase()}
+    >
       <h2 className="text-4xl">{name}.</h2>
       <div className="h-[2px] bg-primary w-full"></div>
     </div>
