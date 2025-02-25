7b62532bdcbdec97f8565a9c53a94f0493d16b97
Thanapon Johdee <sern.dev@gmail.com>
2025-02-25 07:13:23 +0700

    feat: enhance HeroSection layout and improve Navbar animation styles

--- src/app/(landing)/Hero.tsx
+++ src/app/(landing)/Hero.tsx
@@ -4,14 +4,14 @@ import { TextLoop } from '@/components/ui/text-loop';
 export default function HeroSection() {
   return (
     <section className="container px-4 mx-auto w-full md:min-h-[calc(100dvh-360px)]">
-      <div className="flex flex-col my-4 gap-2 w-full text-start text-primary text-2xl md:text-4xl transition">
+      <div className=" my-4 gap-2 w-full h-fit text-start justify-center items-center text-primary text-2xl md:text-4xl transition">
         <div className="inline">
           <span className="">Hi, I&apos;m </span>
           <TextLoop
             interval={4}
-            className="font-bold"
+            className="font-bold md:text-8xl"
           >
-            <span>Thanapon Johdee</span>
+            <span>Thanapon</span>
             <span>Yoke.th</span>
           </TextLoop>
           <br />
--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -4,20 +4,23 @@ import { Menu } from './Menu';
 import * as motion from 'motion/react-client';
 import React from 'react';
 
+const lineStyle: React.CSSProperties = {
+  height: '4px',
+  background: 'black',
+};
+
 export function Navbar() {
   return (
     <nav>
-      <div className="px-16 py-4">
+      <div className="px-4 md:px-16 py-4">
         <div className="flex justify-end">
           <motion.div
-            initial={{ width: 0, height: 1 }}
-            animate={{ width: '100%', height: 1 }}
+            initial={{ width: 0 }}
+            animate={{ width: '100%' }}
             transition={{
               duration: 1,
             }}
-            style={{
-              backgroundColor: 'black',
-            }}
+            style={lineStyle}
           />
         </div>
         <div className="flex justify-between p-2 items-center">
@@ -32,14 +35,12 @@ export function Navbar() {
           </div>
         </div>
         <motion.div
-          initial={{ width: 0, height: 1 }}
-          animate={{ width: '100%', height: 1 }}
+          initial={{ width: 0 }}
+          animate={{ width: '100%' }}
           transition={{
             duration: 1,
           }}
-          style={{
-            backgroundColor: 'black',
-          }}
+          style={lineStyle}
         />
       </div>
     </nav>
